1028154dd9d58539ddc3d1744fb96d6d75eb6a59
Aaron <adub65@gmail.com>
2020-05-18 16:37:52 -0400

    initialized scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,10 @@
 # This file will never use puts
 
 class Scraper
+  attr_accessor :doc
+
+  def initialize
+    @doc = Nokogiri::HTML(open(https://www.dcnr.pa.gov/StateParks/FindAPark/Pages/default.aspx))
+  end
+
 end
